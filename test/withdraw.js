f9fcb9a07204dc442e7ae5794ec3a59e01fbafdb
Peter Polman <peter@peterpolman.nl>
2018-06-29 14:44:14 +0200

    Implemented test for first withdraw

--- test/withdraw.js
+++ test/withdraw.js
@@ -22,8 +22,44 @@ contract('OpenSocialDAICO', async (accounts) => {
       assert(LockedTokensInstance !== undefined, 'has LockedTokensInstance instance');
     });
 
-    it("Team wallet should be able to withdraw 10% of total ETH contributed immediately after finalizing the successful crowdsale.", async() => {
-      // Write test
+    it("Crowdsale should reach soft cap and set the fund in Withdraw Mode.", async () => {
+      let crowdSaleStartTime = await OpenSocialDAICOInstance.SALE_START_TIME.call();
+      let crowdSaleEndTime = await OpenSocialDAICOInstance.SALE_END_TIME.call();
+      var blocktime = await web3.eth.getBlock('latest').timestamp;
+
+      await timeTravel((parseInt(crowdSaleStartTime) + 86400) - blocktime); // 86400 seconds == 1 day
+
+      for (let i = 10; i < 25; i++) {
+        await OpenSocialDAICO.at(OpenSocialDAICO.address).addToWhiteList(web3.eth.accounts[i]);
+
+        for (let j = 0; j < 5; j++) {
+          await OpenSocialDAICO.at(OpenSocialDAICO.address).sendTransaction({
+              from: web3.eth.accounts[i],
+              to: OpenSocialDAICO.address,
+              value: web3.toWei(20, "ether")
+          });
+
+          console.log('... #' + i + ' contributed 20 ETH [' + (j + 1) + 'x]');
+        }
+      }
+
+      var blocktime = web3.eth.getBlock('latest').timestamp;
+
+      await timeTravel(crowdSaleEndTime - blocktime);
+
+      await OpenSocialDAICOInstance.finalizeCrowdsale();
+
+      let state = (await PollManagedFundInstance.state.call()).valueOf();
+
+      assert.equal(state, 2, "The current state is " + state + " and not " + 2 + ".");
+    });
+
+    it("Team wallet should be able to withdraw 10% of total ETH contributed.", async() => {
+      let totalEtherContributed = web3.fromWei( await OpenSocialDAICOInstance.totalEtherContributed.call() );
+      let calculatedFirstTimeWithdraw = totalEtherContributed.valueOf() * 0.1;
+      let firstWithdrawAmount = web3.fromWei( await PollManagedFundInstance.firstWithdrawAmount.call() );
+
+      assert.equal(firstWithdrawAmount.valueOf(), calculatedFirstTimeWithdraw, "First Withdraw Amount is not 10% of the total ETH contributed.");
     });
 
 });
