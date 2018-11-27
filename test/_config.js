671be872d726bdbbfe505759937e3e85af600165
Peter Polman <peter@peterpolman.nl>
2018-11-27 10:01:34 +0100

    Finished automated tests for refund poll

--- test/_config.js
+++ test/_config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  unusedWallets: 10,
+  contributors: 335,
+  refundYes: 334,
+  refundNo: 1,
   privateSaleHardCap: 655,
   softCap: 1500,
-  hardCap: 2500,
+  hardCap: 33500,
   tokenPriceNum: 10000,
   tokenPriceDenom: 1,
   bnbTokenPriceNum: 300,
--- test/refundPoll.js
+++ test/refundPoll.js
@@ -26,20 +26,17 @@ contract('THXTokenDAICO', async (accounts) => {
     });
 
     it("Contributors should contribute as much as the hard cap", async() => {
-      let crowdSaleStartTime = await THXTokenDAICOInstance.BONUS_WINDOW_3_END_TIME();
+      let crowdSaleStartTime = await THXTokenDAICOInstance.SALE_START_TIME();
 
       await timeTravel((parseInt(crowdSaleStartTime) + 86400) - (web3.eth.getBlock('latest').timestamp)); // 86400 seconds == 1 day
 
-      for (let i = 10; i < 35; i++) {
+      for (let i = config.unusedWallets; i < (config.contributors + config.unusedWallets); i++) {
         await THXTokenDAICOInstance.addToLists(accounts[i], true, false, false, false);
         await THXTokenDAICOInstance.sendTransaction({
             from: accounts[i],
             to: THXTokenDAICO.address,
             value: web3.toWei(100, "ether")
         });
-
-        console.log( web3.fromWei(await THXTokenInstance.balanceOf(web3.eth.accounts[i])).valueOf() );
-        console.log( web3.fromWei(await THXTokenInstance.totalSupply()).valueOf() );
       }
 
       let balance = (await web3.eth.getBalance(PollManagedFund.address)).valueOf();
@@ -54,8 +51,6 @@ contract('THXTokenDAICO', async (accounts) => {
       await timeTravel(parseInt(crowdSaleEndTime) - (web3.eth.getBlock('latest').timestamp));
 
       await THXTokenDAICOInstance.finalizeCrowdsale();
-      console.log( web3.fromWei(await THXTokenInstance.balanceOf(web3.eth.accounts[10])).valueOf() );
-      console.log( web3.fromWei(await THXTokenInstance.totalSupply()).valueOf() );
       let state = (await PollManagedFundInstance.state()).valueOf();
 
       assert.equal(state, 2, "The current state is " + state + " and not " + 2 + ".");
@@ -102,20 +97,17 @@ contract('THXTokenDAICO', async (accounts) => {
       let refundPollAddress = await PollManagedFundInstance.refundPoll();
       let yesCounter = await RefundPollInstance.yesCounter();
 
-      for (var i = 10; i < 34; i++) {
+      for (var i = config.unusedWallets; i < (config.unusedWallets + config.refundYes); i++) {
         let tx = await web3.eth.sendTransaction({
           from: accounts[i],
           to: refundPollAddress,
           data: "0x4b9f5c980000000000000000000000000000000000000000000000000000000000000001",
           gas: 200000
         });
-        console.log( web3.fromWei(await THXTokenInstance.balanceOf(web3.eth.accounts[i])).valueOf() );
-        console.log( web3.fromWei(await RefundPollInstance.yesCounter()).valueOf() );
         assert(tx != null);
       }
 
       let newYesCounter = await RefundPollInstance.yesCounter();
-
       assert(newYesCounter > yesCounter, 'no YES votes added');
     });
 
@@ -130,19 +122,16 @@ contract('THXTokenDAICO', async (accounts) => {
       let refundPollAddress = await PollManagedFundInstance.refundPoll();
       let noCounter = await RefundPollInstance.noCounter();
 
-      for (var i = 34; i < 35; i++) {
+      for (var i = (config.unusedWallets + config.refundYes); i < (config.unusedWallets + config.refundYes + config.refundNo); i++) {
         let tx = await web3.eth.sendTransaction({
           from: accounts[i],
           to: refundPollAddress,
           data: "0x4b9f5c980000000000000000000000000000000000000000000000000000000000000000",
           gas: 200000
         });
-        console.log( web3.fromWei(await THXTokenInstance.balanceOf(web3.eth.accounts[i])).valueOf() );
-        console.log( web3.fromWei(await RefundPollInstance.noCounter()).valueOf() );
         assert(tx != null);
       }
       let newNoCounter = await RefundPollInstance.noCounter();
-
       assert(newNoCounter > noCounter, 'no NO votes added');
     });
 
@@ -160,19 +149,19 @@ contract('THXTokenDAICO', async (accounts) => {
       let holdEndTime = await RefundPollInstance.holdEndTime();
       var isWithdrawEnabled = await PollManagedFundInstance.isWithdrawEnabled();
       var state = await PollManagedFundInstance.state();
-
       assert(state.valueOf() == 2, "Poll should still be in withdraw mode");
       assert(isWithdrawEnabled == true, "Poll should still be in withdraw mode");
+      var secondRefundPollDate = await PollManagedFundInstance.secondRefundPollDate();
 
       await timeTravel(parseInt(holdEndTime) - (web3.eth.getBlock('latest').timestamp));
 
       await RefundPollInstance.tryToFinalize();
 
       var isWithdrawEnabled = await PollManagedFundInstance.isWithdrawEnabled();
-      let secondRefundPollDate = await PollManagedFundInstance.secondRefundPollDate();
+      var secondRefundPollDate = await PollManagedFundInstance.secondRefundPollDate();
 
-      assert(isWithdrawEnabled == false, "Withdrawing should be disabled");
       assert(secondRefundPollDate != 0, "second refund poll date should be set");
+      assert(isWithdrawEnabled == false, "Withdrawing should be disabled");
     });
 
     /*********************************************************************************************************
@@ -185,7 +174,7 @@ contract('THXTokenDAICO', async (accounts) => {
     it("A token holder should be able to create the second refund poll on the correct date.", async() => {
       let secondRefundPollDate = await PollManagedFundInstance.secondRefundPollDate();
 
-      await timeTravel(parseInt(secondRefundPollDate) - (web3.eth.getBlock('latest').timestamp));
+      await timeTravel(parseInt(secondRefundPollDate)- (web3.eth.getBlock('latest').timestamp));
 
       var refundPollAddress = await PollManagedFundInstance.refundPoll();
 
@@ -207,7 +196,7 @@ contract('THXTokenDAICO', async (accounts) => {
       let refundPollAddress = await PollManagedFundInstance.refundPoll();
       let yesCounter = await RefundPollInstance.yesCounter();
 
-      for (var i = 10; i < 25; i++) {
+      for (var i = config.unusedWallets; i < (config.unusedWallets + config.refundYes); i++) {
         let tx = await web3.eth.sendTransaction({
           from: accounts[i],
           to: refundPollAddress,
@@ -226,7 +215,7 @@ contract('THXTokenDAICO', async (accounts) => {
       let refundPollAddress = await PollManagedFundInstance.refundPoll();
       let noCounter = await RefundPollInstance.noCounter();
 
-      for (var i = 25; i < 35; i++) {
+      for (var i = (config.unusedWallets + config.refundYes); i < (config.unusedWallets + config.refundYes + config.refundNo); i++) {
         let tx = await web3.eth.sendTransaction({
           from: accounts[i],
           to: refundPollAddress,
@@ -250,7 +239,6 @@ contract('THXTokenDAICO', async (accounts) => {
       assert(state.valueOf() == 2, "Poll should be in withdraw mode");
       assert(isWithdrawEnabled == false, "Poll should be in withdraw mode");
 
-
       await timeTravel(parseInt(endTime) - (web3.eth.getBlock('latest').timestamp));
 
       var tx = await RefundPollInstance.tryToFinalize();
@@ -261,5 +249,18 @@ contract('THXTokenDAICO', async (accounts) => {
       assert(state.valueOf() == 3, "Poll should be in refund mode");
     });
 
+    it("A token holder should be able to refund what is left of the payment", async () => {
+      var balanceBeforeRefund = web3.fromWei(await web3.eth.getBalance(web3.eth.accounts[10]), "ether").valueOf();
+
+      var tx = await PollManagedFundInstance.refundTokenHolder({
+        from: web3.eth.accounts[10]
+      });
+
+      var balanceAfterRefund = web3.fromWei(await web3.eth.getBalance(web3.eth.accounts[10]), "ether").valueOf();
+      var thxBalance = (await THXTokenInstance.balanceOf(web3.eth.accounts[10])).valueOf();
+
+      assert(balanceAfterRefund > balanceBeforeRefund, "Contributor should have more ETH than before refund");
+      assert(thxBalance == 0, "Contributor should have no more THX left");
+    });
 
 });
